9eb1db36e3f5bf7fb09cf00d8b345759e34d823d
Aaditya Sahay <aadityasahay@economist.com>
2021-01-18 23:58:32 +0530

    decodes before passing to prism

--- src/lib/pre.ts
+++ src/lib/pre.ts
@@ -1,5 +1,6 @@
 import { parse } from 'node-html-parser'
 import Prism from 'prismjs'
+import he from 'he'
 
 // refactor this later
 export const prismifyHTML = (html: string): string => {
@@ -11,7 +12,7 @@ export const prismifyHTML = (html: string): string => {
     const code = parse(pre.childNodes[0].rawText).querySelector('code')
     const lang = code.classNames.find((elem) => elem.startsWith('language'))?.split('-')[1]
     if (lang) {
-      const prismed = Prism.highlight(code.innerText, Prism.languages[lang], lang)
+      const prismed = Prism.highlight(he.decode(code.innerText), Prism.languages[lang], lang)
       code.set_content(prismed)
       pre.set_content(code)
       pre.setAttribute('class', `language-${lang}`)
