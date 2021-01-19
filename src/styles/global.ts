fdd6c88c8d9708911b6983b6a04eb5256bd3340c
Liam Dyer <liamcyder@gmail.com>
2021-01-19 09:49:45 -0500

    Use local variable roboto slab

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,13 @@
 import { css } from '@emotion/react'
 
 export const global = css`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+  @font-face {
+    font-family: 'Roboto Slab';
+    src: url('/fonts/RobotoSlab-Variable.woff2') format('woff2 supports variations'),
+      url('/fonts/RobotoSlab-Variable.woff2') format('woff2-variations');
+    font-weight: 100 1000;
+    font-stretch: 25% 151%;
+  }
 
   * {
     margin: 0;
